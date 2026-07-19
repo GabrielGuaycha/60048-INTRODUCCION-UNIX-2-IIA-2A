04e6dc2de576efbdcd480e22fe4f3ba748021956
GabrielGuaycha <gaguaychaes@uide.edu.ec>
2026-07-19 12:36:26 -0500

    wip: rute

---
+++ s2.sh
@@ -0,0 +1,18 @@
+# ssh me permite conectarme a un servidor remotamente
+ssh bandit0@bandit.labs.overthewire.org -p 22220
+# clear me permite limpiar mi termina
+clear
+# me da el historial de comandos ejecutados
+history
+# pwd ruta 
+pwd
+# lista de archivos y directorios 
+ls 
+# listas en un ecosistema de BSD como es mac para ver en listas incluyendo ocultos es con ls -la
+ls -la 
+# el punto simboliza el directorio actual
+cd . 
+# el punt punto simboliza la carpeta madre. 
+cd . . 
+# ese tomando nos guia a la carpeta que elegimos en el comando y nos indica todas las carpetas incluyendo ocultas
+ls -a /Users/gabriel/Documents/GitHub/60048-INTRODUCCION-UNIX-2-IIA-2A/Carpeta/carpeta_hija
